ba71a7137c19dc47adddaa2099352e3704e4ae4d
Josh <joshua.childs01@outlook.com>
2023-09-07 11:43:36 +1000

    raylib base

--- src/chroma-viz.c
+++ src/chroma-viz.c
@@ -1,23 +1,22 @@
-#include <gtk/gtk.h>
-
-static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window;
-
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-
-    gtk_window_present(GTK_WINDOW(window));
-}
+#include <raylib.h>
 
 int main(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
+    const int screen_width = 800;
+    const int screen_height = 450;
 
-    app = gtk_application_new("chroma.viz", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    InitWindow(screen_width, screen_height, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
-    return status;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
